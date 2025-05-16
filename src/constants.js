335a18be8245b4b2a8d5b9c36d4680908c8c3227
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2025-05-16 12:28:14 +0530

    change port

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 // require("dotenv").config();
 
 export const CONSTANTS = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
 };
