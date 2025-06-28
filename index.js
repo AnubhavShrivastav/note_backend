92a08982b93ebfd7b11bb61883c2ffd36c819c66
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2025-06-28 23:43:31 +0530

    add cors URL or Remove  URL

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const routes = require("./src/router");
 const { CONSTANTS } = require("./src/constants");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://note-meapp.netlify.app",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(bodyParser.json());
 
--- src/router.js
+++ src/router.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const userRouter = require("./user/routes");
 const noteRouter = require("./notes/routes");
-const cors = require("cors");
 const authMiddleware = require("./middleware");
 
 const router = express.Router();
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: "https://note-meapp.netlify.app",
-  credentials: true,
-}));
+
 
 router.use("/users", userRouter);
 router.use("/notes", authMiddleware, noteRouter);
